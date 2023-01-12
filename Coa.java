1439d85ddd4bf4acc36fbe9f0437958552810c28
Cynthia Moraa <89919637+Abugahh@users.noreply.github.com>
2023-01-12 17:19:08 +0300

    Update Coa.java

--- Coa.java
+++ Coa.java
@@ -1,1 +1,25 @@
-public static void main 
+import java.util.Random;
+import java.text.DecimalFormat;
+
+public class DecimalToBinary {
+    static int NUM_RANDOM_NUMS = 30;
+    static int MAX_DECIMAL_PLACES = 3;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        // Generate and convert random numbers to binary notation
+        for (int i = 0; i < NUM_RANDOM_NUMS; i++) {
+            double decimalNum = rand.nextDouble() * 100;
+            decimalNum = Double.valueOf(df.format(decimalNum));
+            String binaryNum = Long.toBinaryString(Double.doubleToLongBits(decimalNum));
+            String remarks;
+            if(binaryNum.charAt(0) == '0'){
+                remarks = "exactly";
+            }else{
+                remarks = "approximately";
+            }
+            System.out.println("
+
